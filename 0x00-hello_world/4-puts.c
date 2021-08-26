5a70f5a8f74f67be247148335e17f35a1eac39f1
doreenbett <doreenbett001@gmail.com>
2021-08-26 14:48:12 +0000

    Adding puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+* main - Printing string using puts
+* Return: 0
+**/
 
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n"
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
 }
