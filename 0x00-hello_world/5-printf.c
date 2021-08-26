fe801a8ab68d72d87a54b6c9c639c6a47186cde6
doreenbett <doreenbett001@gmail.com>
2021-08-26 15:25:28 +0000

    Adding prints

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /*
-* main -Print string using printf
+* main - Print string using printf
 * Return: 0
 */
 
 int main(void)
 {
-printf("\"with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 }
