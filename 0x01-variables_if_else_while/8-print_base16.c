afa8bb015f859aa2b155a3ccd5c692b50d591cdb
doreenbett <doreenbett001@gmail.com>
2021-08-27 20:56:10 +0000

    Adding base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int num;
 char letter;
 for (num = 0; num < 10; num++)
 putchar((num % 10) + '0');
-for (num = 'a'; letter <= 'f'; letter++)
+for (letter = 'a'; letter <= 'f'; letter++)
 putchar(letter);
 putchar('\n');
 return (0);
