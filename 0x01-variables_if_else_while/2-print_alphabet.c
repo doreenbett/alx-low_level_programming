c7fd2e6d62681f36aedf30850d7b1f9662cf9c01
doreenbett <doreenbett001@gmail.com>
2021-08-27 19:33:20 +0000

    Adding variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,9 @@
 */
 int main(void)
 {
-char letter:
+char letter;
 for (letter = 'a'; letter <= 'z'; letter++)
 putchar(letter);
-for (letter = 'A'; letter <= 'z'; letter++)
-putchar(letter);
-putchar('\n);
+putchar('\n');
 return (0);
 }
