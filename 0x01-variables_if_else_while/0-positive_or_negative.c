f10f31881c14f67d48837dd3518bcf3f5ef0ab2b
doreenbett <doreenbett001@gmail.com>
2021-08-27 16:54:55 +0000

    Adding variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,11 +7,11 @@ int main(void)
 	int n;
 
 if (n > 0)
-printf("%dis positive\n", n);
+printf("%d is positive\n", n);
+else if (n < 0) 
+printf("%d is negative\n", n);
 else 
 printf("%d is zero\n", n);
-else if (n < 0)
-printf("%d is negative\n", n);
 
 return (0);
 }
