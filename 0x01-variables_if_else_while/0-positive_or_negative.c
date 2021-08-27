5be15e4bd2414225bdd5bf0128019cd1b4c7b544
doreenbett <doreenbett001@gmail.com>
2021-08-27 16:24:03 +0000

    Adding variables

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+if (n > 0)
+printf("%dis positive\n", n);
+else if (n < 0)
+printf("%d is negative\n", n);
+else
+printif("%d is zero\n", n);
+
+return (0);
+}
