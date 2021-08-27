e8bf87cf9d31d0dbc5d0bd71e4fabf7f1852d074
doreenbett <doreenbett001@gmail.com>
2021-08-27 17:27:02 +0000

    Adding variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,14 +6,17 @@
 /* betty style doc for function main goes there */
 int main(void)
 {
-	int n;
+int n;
 
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
 if (n > 0)
-printf("%d is positive\n", n);
-else if (n < 0) 
-printf("%d is negative\n", n);
-else 
-printf("%d is zero\n", n);
+printf("%d is positive\n",);
+else if (n < 0)
+printf("%d is negative\n",);
+else
+printf("%d is zero\n",);
 
 return (0);
 }
