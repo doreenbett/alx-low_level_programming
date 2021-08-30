49855188bc6ed6f841bfeaa52b916bdc2121eaec
doreenbett <doreenbett001@gmail.com>
2021-08-30 08:19:31 +0000

    Adding comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
-*main - Prints all numbers with combinations of three
+*main - Prints combination of three digits
 *
 *Return: 0
 */
 int main(void)
 {
-int digit1, digit2, digit3;
+int = digit1, digit2, digit3;
+
 for (digit1 = 0; digit1 < 8; digit1++)
 {
-for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+for (digit2 = digit1 +1; digit2 < 9; digit2++)
 {
-for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+for (digit3 = digit2 +1; digit2 <10; digit3++)
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 putchar((digit3 % 10) + '0');
-if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+if(digit1 == 7 && digit2 == 8 && digit3 == 9)
 continue;
 putchar(',');
 putchar(' ');
@@ -24,6 +25,5 @@ putchar(' ');
 }
 }
 putchar('\n');
-
 return (0);
-}                        
+}    
