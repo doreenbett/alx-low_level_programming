41deac0179d0e9e1e2e54778b01c7c55ef37f50e
doreenbett <doreenbett001@gmail.com>
2021-08-31 11:07:45 +0000

    Adding holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main - Prints holberton
+*
+*Return: 0
+*/
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('0');
+	_putchar('n');
+	_putchar('\n');
+	return (0);
+}
