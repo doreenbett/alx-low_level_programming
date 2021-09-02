a1bea24684dd7d067942ba48508b61d085b066e8
doreenbett <doreenbett001@gmail.com>
2021-09-02 21:37:05 +0000

    Adding main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,23 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <main.h>
 /**
-* positive_or_negative - Tests function that prints positive or negative
-*@i: The integer being used to  perform tests
-*Return: return the result of test
+* positive_or_negative - Prints positive or negative results
+* Description: change to positive or negative
+* @i: Integer to be used
 */
-
 void positive_or_negative(int i)
 {
 
-	i = 0;
-	positive_or_negative(i);
-	return (i);
 
+srand(time(0));
+i = rand() - RAND_MAX / 2;
+/* your code goes there */
+if (i > 0)
+printf("%d is positive\n", i);
+else if (i < 0)
+printf("%d is negative\n", i);
+else
+printf("%d is zero\n", n);
+return (0);
 }
