29230baf273c9a592fff3b33489ba7f7de44a407
doreenbett <doreenbett001@gmail.com>
2021-09-02 21:19:38 +0000

    Adding main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
 * positive_or_negative - Tests function that prints positive or negative
+*@i: The integer being used to  perform tests
 */
 
 void positive_or_negative(int i)
@@ -8,5 +9,6 @@ void positive_or_negative(int i)
 
 	i = 0;
 	positive_or_negative(i);
+	return (i);
 
 }
