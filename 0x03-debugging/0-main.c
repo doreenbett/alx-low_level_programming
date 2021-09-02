c63094670c763cf6128e131e6e4fb4762a66afd1
doreenbett <88307192+doreenbett@users.noreply.github.com>
2021-09-03 00:36:04 +0300

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,1 @@
-#include "main.h"
-/**
-* positive_or_negative - Tests function that prints positive or negative
-*@i: The integer being used to  perform tests
-*Return: return the result of test
-*/
 
-void positive_or_negative(int i)
-{
-
-	i = 0;
-	positive_or_negative(i);
-	return (i);
-
-}
