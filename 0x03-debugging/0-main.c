9e9001cd36d50ce8c07b6efe695e833dee14e3cb
doreenbett <doreenbett001@gmail.com>
2021-09-02 16:03:57 +0000

    Adding main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,6 @@
 #include "main.h"
 /**
-<<<<<<< HEAD
-* positive_or_negative - tests function that prints positive or negative
-* Return: 
+* positive_or_negative - Tests function that prints positive or negative
 */
 
 void positive_or_negative(int i)
@@ -10,20 +8,5 @@ void positive_or_negative(int i)
 
 	i = 0;
 	positive_or_negative(i);
-	return;
 
-	
-=======
-* main - tests function that prints if integer is positive or negative
-* Return: 0
-*/
-void positive_or_negative(int i);
-{
-	int i
-	
-	i = 98;
-	positive_or_negative(i);
-
-	return (0);
->>>>>>> eb01e22a8c4fb4bc578c55e324ef6ccc88fd5b25
 }
