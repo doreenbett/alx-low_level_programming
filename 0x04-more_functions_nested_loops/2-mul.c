cfd435a60c413355a45a2782a205ab77e9114b12
doreenbett <doreenbett001@gmail.com>
2021-09-02 20:19:54 +0000

    Adding mul function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*mul - Writes a function that multiplies to integers
+*@a and @b: These are the two integers
+*Return: 0
+*/
+int mul(int a, int b)
+{
+	printf("%d\n", mul(a, b))
+	return (0);
+}
