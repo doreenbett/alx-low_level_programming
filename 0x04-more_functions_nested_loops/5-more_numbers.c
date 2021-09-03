b45722cd4dd59d516fcc5901accccb351b27c330
doreenbett <doreenbett001@gmail.com>
2021-09-03 06:32:24 +0000

    Adding more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*more_numbers - Prints 10 times the numbers
+*
+*/
+void more_numbers(void)
+{
+	int num
+	int i
+
+	for (i = 0; i <= 10; i++)
+
+	for (num = 0; num <= 14; num++)
+	_putchar(num);
+	_putchar('\n');
+}
