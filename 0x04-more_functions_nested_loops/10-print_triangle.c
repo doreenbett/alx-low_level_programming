417f1bb54add330460ef790f015848d4974200e5
doreenbett <doreenbett001@gmail.com>
2021-09-03 20:35:58 +0000

    Adding triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*print_triangle - prints a triangle
+*@size: size of triangle
+*
+*/
+void print_triangle(int size)
+{
+int i;
+int j;
+
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 1; j <= size; j++)
+
+if (j >= size - i)
+{
+_putchar('#');
+}
+esle
+{
+_putchar(' ');
+}
+}
+_putchar('\n')
+}
+}
