5775623329e6249edadbb0eb1134050351ebc979
doreenbett <doreenbett001@gmail.com>
2021-09-03 20:08:22 +0000

    Adding printline

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,22 @@
 */
 void print_line(int n)
 {
-	char i;
+	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
 	_putchar('\n');
 }
-
+{
 	for (i = 1; i <= n; i++)
 {
 	_putchar('_');
 }
 	_putchar('\n');
+
+	else
+{
+	_putchar('\n');
+}
+}
 }
