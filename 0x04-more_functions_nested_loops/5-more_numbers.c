3a1a6dbc47fe6634a7e89ae10f978208f7ea0db9
doreenbett <doreenbett001@gmail.com>
2021-09-03 17:59:15 +0000

    Adding more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,9 +8,12 @@ void more_numbers(void)
 	int i, j;
 
 
-	for (i = '0'; i <= '10'; i++)
+	for (i = '0'; i <= '9'; i++)
 
 	for (j = '0'; j <= '14'; j++)
-	_putchar(num);
-	_putchar('\n');
+	if (j > 9)
+	_putchar((j / 10) + '0');
+	_putchar(((num % 10) + '0');
+
+_putchar('\n');
 }
