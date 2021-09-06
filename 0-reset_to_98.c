4471c05d90ef66497a8ac07ee99ade264a1491c3
doreenbett <doreenbett001@gmail.com>
2021-09-06 19:31:14 +0000

    Adding 0-reset_to_98.c

---
+++ 0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* reset_to_98 - function that takes pointer to an int
+* @n: used as a pointer
+*/
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = 98
+	n = &p
+	printf("p=%d\n", p);
+	_putchar(&p);
+	printf("p=%d\n", p);
+}
