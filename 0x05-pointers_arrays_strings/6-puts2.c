788a3859fe4ee706249e2f986ea0c93ddf127dae
doreenbett <doreenbett001@gmail.com>
2021-09-07 18:34:59 +0000

    Adding puts

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* puts2 - prints everycharacter of a string,starting with first
+* @str: pointer integer
+*/
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putcar('\n');
+}
