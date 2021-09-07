afa418c57c78dd86c78cb8627c0315c8a7a30fcd
doreenbett <doreenbett001@gmail.com>
2021-09-07 18:53:23 +0000

    Adding putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+* puts_half - prints half of a string
+* @str: pointer of an integer
+*/
+void puts_half(char *str);
+{
+	int i = 0;
+	int n;
+
+	while (str[i] != '\0')
+	{
+
+		i++;
+
+	}
+	if (i % 2 != 0)
+	{
+
+		n = (i - 1) / 2;
+	}
+	else
+	{
+
+		n = i / 2;
+
+	}
+	for (n = i - n; n < i; n++)
+	{
+		_putchar(str[n]);
+	}
+	-putchar('\n');
+}
