6251684f96c59ff4fbac681e4329252a01cbbcaf
doreenbett <doreenbett001@gmail.com>
2021-09-08 21:18:58 +0000

    Adding puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,10 @@ void puts2(char *str)
 	int i = 0;
 
 	while (str[i] != 0)
+		i++;
+	while (i >= 0)
 	{
-		if (i % 2 == 0)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 		i++;
 	}
 	_putchar('\n');
