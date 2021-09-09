a18e7fe738ea56f41f7b11d26079a19fbb861eae
doreenbett <doreenbett001@gmail.com>
2021-09-09 15:29:54 +0000

    Adding toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* *string_toupper - changes lowercase letter to upper
+* @a: pointer
+*
+* Return: character
+*/
+char *string_toupper(char *a);
+{
+	int i = 0;
+
+	while (a[i])
+	{
+		if (a[i] >= 97 && (a[i] <= 122)
+
+			a[i] -= 32;
+		i++;
+	}
+	return (a);
+} 
