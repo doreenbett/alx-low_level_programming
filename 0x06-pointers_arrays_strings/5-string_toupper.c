76d2d0c03105587b4aa2bfb53d9636f04f60f4b0
doreenbett <doreenbett001@gmail.com>
2021-09-14 09:59:12 +0000

    Fixing codes

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* string_toupper - changes lowercase letter to upper
+*string_toupper - changes lowercase letter to upper
 * @a: pointer
 *
 * Return: pointer of a string
 */
-char *string_toupper(char *a);
+char *string_toupper(char *a)
 {
 	int i = 0;
 
@@ -13,8 +13,9 @@ char *string_toupper(char *a);
 	{
 		if ((*(a + i) >= 97) && (*(a + i) <= 122))
 
-			(*(a + i) - 32;
+			*(a + i) = *(a + i) -32;
 			i++;
 	}
 	return (a);
 }
+
