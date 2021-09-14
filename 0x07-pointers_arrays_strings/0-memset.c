99632317748582772088c8123cee7503a46c2f31
doreenbett <doreenbett001@gmail.com>
2021-09-14 09:02:43 +0000

    Fixing codes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,17 +9,15 @@
 * Return: a pointer to memory area
 */
 char *_memset(char *s, char b, unsigned int n)
-{
 
+{
 	unsigned int i;
 
 	i = 0;
 
-	while (i < n)
-	{
-		*(s + 1) = b;
-		i++;
-	}
+	for (i = 0; i < n; i++)
+		*(s + i) = b;
+
 
 	return (s);
 }
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 #include <string.h>
 int _putchar(char c);
-char *_memset(char *s, char b, unsigned int n,
+char *_memset(char *s, char b, unsigned int n);
+#endif
