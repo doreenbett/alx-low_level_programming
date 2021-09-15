2ca315e013be8529a81b020a886360253d1782ba
doreenbett <doreenbett001@gmail.com>
2021-09-15 13:49:45 +0000

    Adding rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_print_rev_recursion - prints a string in reverse
+*
+*@s: pointer to a string
+*
+*/
+void _print_rev_recursion(char *s)
+{
+	int i = 0;
+	int j = 0;
+
+	if (i >= j)
+	{
+		_putchar(s);
+		i--;
+	}
+}
+		
