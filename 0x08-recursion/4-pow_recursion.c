3f5adcbec7d2dd11071e53ab45b59b3eff2fc9a7
doreenbett <doreenbett001@gmail.com>
2021-09-15 19:07:29 +0000

    Adding pow

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,13 +10,15 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	if (x == 0)
-		return (0);
+	if (x <= 0)
+		return (-1);
 	if (y <= 1)
 		return (1);
 	if (x < 0)
 		return (-1);
-	if (y == 0)
+	if (y <= 0)
+		return (-1);
+	if (x == 0)
 		return (0);
 
 	return (_pow_recursion(x, y));
