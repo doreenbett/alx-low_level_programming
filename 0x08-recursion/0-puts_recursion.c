e97a8149856aabe140fe92532d9e0a3bc6ef3fdf
doreenbett <doreenbett001@gmail.com>
2021-09-15 12:29:19 +0000

    Adding recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*_puts_recursion - prints a string
+*@s: pointer to a string
+*
+*/
+void _puts_recursion(char *s)
+{
+	int i = 0;
+	int n = 0;
+
+	if (i < n)
+	{
+		s++;
+		i++;
+	}
+}
+
