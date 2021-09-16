1adc41de7e6c05f0f3ae774956d3af8405794aee
doreenbett <doreenbett001@gmail.com>
2021-09-16 11:47:40 +0000

    Adding sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,30 @@
+
+
 #include "main.h"
 /**
 *_sqrt_recursion - returns thenatural square root
-*@n: the value to get square root
-*
-*Return: natural square root of number
+*@num: the value to get square root
+*@r: the integer to check and square
+*Return: root of number
+*/
+int root(int r, int num)
+{
+
+	if ((r * r) == num)
+		return (1);
+	if (r == num / 2)
+		return (-1);
+	return (root(num, r + 1));
+}
+/**
+*_sqrt_recursion - returns natural square root of a number
+*@n: the value to get sqrt
+*Return: returns natursl sqrt of a number
 */
 int _sqrt_recursion(int n)
 {
+	int r =0;
+
 	if (n < 0)
 		return (-1);
 	if (n == 0)
@@ -14,5 +32,6 @@ int _sqrt_recursion(int n)
 	if (n <= 1)
 		return (1);
 
-	return (n * _sqrt_recursion(n + 1));
+	return (root(n, r));
 }
+
