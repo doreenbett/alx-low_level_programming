28061f784af2c2ae0450624edbefd0b08ae28d1e
doreenbett <doreenbett001@gmail.com>
2021-09-16 12:16:01 +0000

    Adding palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*is_palindrome - returns 1 if a string is a palindrome
+*@s: pointer to a string
+*
+*Return: 1 if its a palindrome and 0 otherwise
+*/
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	if (*s != '\0')
+		return (0);
+	return (is_palindrome(s + 1));
+}
