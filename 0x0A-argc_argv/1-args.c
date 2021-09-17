a6cb17634e790ca591aa21cd9f9b7eb1121e0556
doreenbett <doreenbett001@gmail.com>
2021-09-17 11:59:17 +0000

    Adding args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - prints the number or arguments passed to it
+*@argc: contains size of array
+*@argv: contains size of argc
+*
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%d\n", argc);
+		printf("%s\n", argv[i]);
+
+	}
+	return (0);
+}
