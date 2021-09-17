60c54c937f657c0d95511731e548053ddaceda8b
doreenbett <doreenbett001@gmail.com>
2021-09-17 13:11:16 +0000

    Adding args2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,8 +12,7 @@ int main(int argc, char **argv)
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("argc = %d\n", argc);
-		printf("argv[%d] = %s\n", i, argv[i]);
+		printf("%s\n", argv[i]);
 	}
 	return (0);
 }
