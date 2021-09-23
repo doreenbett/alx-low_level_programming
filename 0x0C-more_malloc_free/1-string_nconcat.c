76f657a73bc94bafda36ccae67936d27d108638c
doreenbett <doreenbett001@gmail.com>
2021-09-23 19:53:34 +0000

    Adding concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,18 +20,22 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		i++;
 	while (s2[k])
 		k++;
+
 	if (n >= k)
 		l = i + k;
 	else
-		l = i + k;
+		l = i + n;
+
 	str = malloc(sizeof(char) * l + 1);
 	if (str == NULL)
 		return (NULL);
+
 	k = 0;
 	while (j < l)
 	{
 		if (j <= i)
 			str[j] = s1[i];
+
 		if (j >= i)
 		{
 			str[j] = s2[k];
