05bea34e15fb1f95b65247f507e17b113415100a
doreenbett <doreenbett001@gmail.com>
2021-09-23 17:33:32 +0000

    Adding malloc checks

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
 *malloc_checked - function that allocates memory using malloc
-*@b: inteer value
-*
+*@b: integer value
+*Return: pointer to memory
 */
 void *malloc_checked(unsigned int b)
 {
-        int *p;
+	int *p;
 
-        p = malloc(sizeof(int) * 98);
-        if (b == NULL)
-                exit (0);
-        return (98);
+	p = malloc(sizeof(int) * 98);
+	if (b == 0)
+		exit(0);
+
+return (p);
 }
