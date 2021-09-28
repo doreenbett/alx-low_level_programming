de11989f7f575d0aad2c11ac1de1bb8db00712b5
doreenbett <doreenbett001@gmail.com>
2021-09-28 21:52:28 +0000

    Fixing codes

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
 *main - prints the name of the file it was compiled from
-*@argc: contains the number of aruments passed in argv
-*@argv: size of argc
+*
 *Return: 0
 */
 int main(void)
