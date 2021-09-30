3a2a669581ceff735ea222b35a9e0aa3bc10ccff
doreenbett <doreenbett001@gmail.com>
2021-09-30 18:29:46 +0000

    Adding print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+*print_numbers - prints numbers folowed by new line
+*@separator: is the string to be printed
+*@n: number of integers passed to the function
+*
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list ap;
+
+	va_start (ap, n);
+
+	if ((separator == NULL) || (n == 0))
+		return;
+	for (i = 0; i < n; i++)
+	{
+		va_arg(ap, void);
+	}
+	va_end(ap);
+}
