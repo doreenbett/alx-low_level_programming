456ae789b50e9a44584c9e06b1e396bf0c5551dd
doreenbett <doreenbett001@gmail.com>
2021-09-30 19:10:52 +0000

    Adding printnum

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <string.h>
 /**
 *print_numbers - prints numbers followed by new line
 *@separator: is the string to be printed
@@ -12,7 +13,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list ap;
 
-	va_start (ap, n);
+	va_start(ap, n);
 
 	if ((separator == NULL) || (n == 0))
 		return;
