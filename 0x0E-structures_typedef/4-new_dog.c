5fa4bcb7bfc930b77b6c7515f32ef0e77419a65a
doreenbett <doreenbett001@gmail.com>
2021-10-07 17:15:55 +0000

    Adding linked lists

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 int _strlen(char *s);
 char *_strcpy(char *dest, char *src);
@@ -40,7 +42,7 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 /**
-*new_dog - creates a new
+*new_dog - creates a new dog
 *@name: character pointer
 *@owner: character pointer
 *@age: float
@@ -48,38 +50,33 @@ char *_strcpy(char *dest, char *src)
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *lovely_dog;
-	int name_len = 0, owner_len = 0;
+	dog_t *love_dog;
 
-	if (name != NULL && owner != NULL)
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+
+	love_dog = malloc(sizeof(dog_t));
+	if (love_dog == NULL)
+		return (NULL);
+
+	love_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (love_dog == NULL)
 	{
-		name_len = _strlen(name) + 1;
-		owner_len = _strlen(owner) + 1;
-		lovely_dog = malloc(sizeof(dog_t));
-
-		if (lovely_dog == NULL)
-			return (NULL);
-
-		lovely_dog->name = malloc(sizeof(char) * name_len);
-
-		if (lovely_dog->name == NULL)
-		{
-			free(lovely_dog);
-			return (NULL);
-		}
-
-		lovely_dog->owner = malloc(sizeof(char) * owner_len);
-
-		if (lovely_dog->owner == NULL)
-		{
-			free(lovely_dog->name);
-			free(lovely_dog);
-			return (NULL);
-		}
-
-		lovely_dog->name = _strcpy(lovely_dog->name, name);
-		lovely_dog->owner = _strcpy(lovely_dog->owner, owner);
-		lovely_dog->age;
+		free(love_dog);
+		return (NULL);
 	}
-	return (lovely_dog);
+	love_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (love_dog->owner == NULL)
+	{
+		free(love_dog->name);
+		free(love_dog);
+		return (NULL);
+	}
+	love_dog->name = _strcpy(love_dog->name, name);
+	love_dog->age;
+	love->owner = _strcpy(love_dog->owner, owner);
+
+	return (love_dog);
+}
+	return (love_dog);
 }
