4c068aea75b18932577c8bbc478f7078378b4832
doreenbett <doreenbett001@gmail.com>
2021-10-12 19:07:49 +0000

    Modifying codes

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,16 @@
 */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	int count = 0;
 
-	while (h != NULL)
+	if (h != NULL)
 	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
+		while (h != NULL)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
 	}
-	return (nodes);
+	return (count);
 }
