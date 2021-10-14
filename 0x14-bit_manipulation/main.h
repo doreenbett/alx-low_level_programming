6dfa136a7cf53ee61914f505def8c4fe43f45445
doreenbett <doreenbett001@gmail.com>
2021-10-14 21:13:44 +0000

    Fixing codes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,6 @@
 #define MAIN_H
 #include <math.h>
 #include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
