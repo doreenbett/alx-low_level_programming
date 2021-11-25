019a5c69d7bae0882df7256dc3bb58a2b4e6ce99
doreenbett <doreenbett001@gmail.com>
2021-11-25 19:21:11 +0000

    Adding files

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,21 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* struct dlistint_s - double linked list
+* @n: integer
+* @prev: ponts to the previous node
+* @next: points to the next node
+*/
+typedef struct dlistint_s
+{
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
